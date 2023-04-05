598c7841147a9b55c6099613993beed8ad083a70
Thomas Young <thyo9470@colorado.edu>
2023-04-05 11:00:48 -0700

    Added links and info to coming shroom page to follow insta

--- src/ComingShroom/ComingShroom.js
+++ src/ComingShroom/ComingShroom.js
@@ -1,11 +1,19 @@
 import './ComingShroom.css';
 
+import InstagramIcon from '@mui/icons-material/Instagram';
+import { Container } from '@mui/material';
+
 const ComingShroom = () => {
+
+    const instagramLink = 'https://www.instagram.com/clustermush/';
+
     return (
-        <div id="ComingShroomContainer">
+        <Container id="ComingShroomContainer">
             <img src="/logos/logo.svg" className="App-logo" alt="logo" />
             <h1 id="ComingShroomh1">Coming Shroom</h1>
-        </ div>
+            <p>Funki fungi to encourage appreciation and understanding of our mushroom friends. Follow the Cluster Mush instagram for future updates!</p>
+            <a href={instagramLink}><InstagramIcon fontSize='large'/></a>
+        </Container>
     )
 }
 
